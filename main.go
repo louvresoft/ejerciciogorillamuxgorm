a88fe680ef6c47f8bd2c10fed1c7f4f3ab4ee053
marco_romeromon <marco.romero@eon.com.mx>
2022-07-07 08:28:31 -0500

    Se crea configuracion para conectar a postgresql

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/louvresoft/go-gorm-gorilla-mux/db"
 	"github.com/louvresoft/go-gorm-gorilla-mux/routes"
 	"net/http"
 )
 
 func main() {
+	db.DBConnection()
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHAndler)
 	http.ListenAndServe(":3000", r)
